6de203af0614d3f7ffaa816d31c0babc74f79160
migkast <migkast@hotmail.com>
2024-10-18 18:25:31 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,15 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      fontFamily: {
-        'comic-sans': ['Comic Sans MS', 'cursive'],
+      animation: {
+        float: 'float 3s ease-in-out infinite',
       },
+      keyframes: {
+        float: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-20px)' },
+        }
+      }
     },
   },
   plugins: [],
